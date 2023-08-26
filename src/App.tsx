02c511cd175a6c4d0eaf3a95885103074887f8c8
Diego França <diego.franca92@gmail.com>
2023-08-25 22:44:53 -0300

    feat: add autofocus and remove button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import TodoList from './TodoList'
 
 function App() {
-  const [todoInput, setTodoInput] = useState<string>('')
-  const [todoList, setTodoList] = useState<any[]>([])
-
-  useEffect(() => {
-    let inicialList = [
-      {
-        id: 'fdsfsd',
-        text: 'Todo 1',
-        done: false
-      },
-      {
-        id: 'fdgdg',
-        text: 'Todo 2',
-        done: false
-      }
-    ] as never
-    setTodoList(inicialList)
-  }, [])
 
   return (
     <main>
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,27 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function TodoList() {
   const [todoInput, setTodoInput] = useState<string>('')
-  const [todoList, setTodoList] = useState<{
-    id: string
-    text: string
-    isDone: boolean
-  }[]>([])
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  const inputTodo = useRef<any>(null) 
+  const [todoList, setTodoList] = useState<
+    {
+      id: string
+      text: string
+      isDone: boolean
+    }[]
+  >([])
 
   function addTodo() {
     setTodoList(value => {
-      return [...value, {
-        id: Math.random().toString(),
-        text: todoInput,
-        isDone: false,
-      }]
+      return [
+        ...value,
+        {
+          id: Math.random().toString(),
+          text: todoInput,
+          isDone: false
+        }
+      ]
     })
+    setTodoInput('')
+    inputTodo.current.focus()
+  }
+
+  function removeTodo(id: string) {
+    const newList = todoList.filter(todoItem => todoItem.id !== id)
+    setTodoList(newList)
     setTodoInput('')
   }
 
   useEffect(() => {
-    let inicialList = [
+    const inicialList = [
       {
         id: 'fdsfsd',
         text: 'Todo 1',
@@ -40,19 +54,27 @@ function TodoList() {
     <main>
       <div className='input-box'>
         <input
+          ref={inputTodo}
           type='search'
           placeholder='Cria uma tarefa'
           value={todoInput}
           onChange={e => setTodoInput(e.target.value)}
           autoFocus
         />
-        <button disabled={!todoInput.length} onClick={addTodo}>Nova tarefa</button>
+        <button disabled={!todoInput.length} onClick={addTodo}>
+          Nova tarefa
+        </button>
       </div>
       <ul>
         {todoList.map(todoItem => (
           <li className='todo-item' key={todoItem.id}>
             <input id={todoItem.id} value={todoItem.text} type='checkbox' />
             <label htmlFor={todoItem.id}>{todoItem.text}</label>
+            <button
+              className='btn-remove'
+              onClick={() => removeTodo(todoItem.id)}>
+              x
+            </button>
           </li>
         ))}
       </ul>
