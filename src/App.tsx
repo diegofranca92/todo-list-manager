a33c6ac2bb44dc91ea877ef66c52acbf40f0bfd5
Diego França <diego.franca92@gmail.com>
2023-08-28 23:14:44 -0300

    change pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { Link } from 'react-router-dom'
 import './App.css'
 import TodoIcon from './assets/todo'
+import { useState } from 'react'
 
 function App() {
-
+  const [todoInput, setTodoInput] = useState<string>('')
   return (
     <main className='main-page'>
       <TodoIcon />
       <div>
         <h1>TODO App</h1>
         <h3>Aqui você pode criar suas tarefas da sua forma</h3>
-        <Link to='todos/create'>Nova tarefa</Link>
+        <form className='form-create'>
+          <label htmlFor='todoInput'>Nome da Lista</label>
+          <input
+            id='todoInput'
+            value={todoInput}
+            autoFocus
+            onChange={e => setTodoInput(e.target.value)}
+            type='text'
+            placeholder='Insira o nome de sua Todo List'
+          />
+          <Link to='edit/1'>Criar Lista</Link>
+        </form>
+        {/* <Link to='todos/create'>Nova tarefa</Link> */}
       </div>
-      
     </main>
   )
 }
--- src/pages/TodoCreate.tsx
+++ src/pages/TodoCreate.tsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../App.css'
-import { Link } from 'react-router-dom'
+import TodoList from './TodoList'
+import { useParams } from 'react-router-dom'
 
 function TodoCreate() {
-  const [todoInput, setTodoInput] = useState<string>('')
+  const [todoInput, setTodoInput] = useState<string>('Teste nome da Lista')
+  // const params = useParams()
+
+  // useEffect(() => {
+  //   setTodoInput(params)
+  // }, [params])
 
   return (
-    <form>
-      <label htmlFor="todoInput">Nome da Lista</label>
-      <input id='todoInput' value={todoInput} onChange={e => setTodoInput(e.target.value)} type="text" placeholder='Insira o nome de sua Todo List' />
-      <Link to='edit/1'>Nova Lista</Link>
-    </form>
+    <>
+      <h1>{todoInput}</h1>
+      <TodoList />
+    </>
   )
 }
 
